4ef3e44515bde75d4bb75c773b1a6257f4087750
Mark König <mark.koenig@kleiner-schelm.de>
2021-08-05 21:15:39 +0200

    add testing for single files

---
+++ testSingleFile.py
@@ -0,0 +1,28 @@
+import io
+import os
+import re
+import pathlib
+import time
+
+# test for files
+import reko
+import pc8
+import pc16
+import pc24
+
+# actual path
+actDir = pathlib.Path().resolve()
+
+#create output folder
+output = os.path.join(actDir,'output')
+if not os.path.exists(output):
+    os.mkdir(output)
+
+fileDir = os.path.join(actDir, 'files')
+
+#reko.Loadfile(os.path.join(actDir, 'files', 'Aquarium.REKO') , output)
+pc8.Loadfile(os.path.join(actDir, 'files', 'Dogs1.RKP') , output)
+#pc16.Loadfile(os.path.join(actDir, 'files', 'TinTin.RKP') , output)
+#pc24.Loadfile(os.path.join(actDir, 'files', 'BorisCards1.rkp') , output)
+
+
