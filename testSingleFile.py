8b89d17cdf06d3eb6aaa3f7c55a59a29a394d8b5
Mark König <mark.koenig@kleiner-schelm.de>
2021-08-19 18:34:29 +0200

    Update testSingleFile.py

--- testSingleFile.py
+++ testSingleFile.py
@@ -20,13 +20,10 @@ if not os.path.exists(output):
 
 fileDir = os.path.join(actDir, 'files')
 
-#reko.Loadfile(os.path.join(actDir, 'files', 'Aquarium.REKO') , output)
+reko.Loadfile(os.path.join(actDir, 'files', 'Aquarium.REKO') , output)
 #pc8.Loadfile(os.path.join(actDir, 'files', 'Dogs1.RKP') , output)
 #pc16.Loadfile(os.path.join(actDir, 'files', 'TinTin.RKP') , output)
-pc24.Loadfile(os.path.join(actDir, 'files', 'BorisCards1.rkp') , output)
-
-#reko.Loadfile(os.path.join(actDir, 'files2', 'Default.REKO') , output)
-#reko.Loadfile(os.path.join(actDir, 'files2', 'BB2B02.REKO') , output)
+#pc24.Loadfile(os.path.join(actDir, 'files', 'BorisCards1.rkp') , output)
 
 
 
